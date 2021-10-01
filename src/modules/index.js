65340d601ad8f0c63fbb617b6c6c9e916fe9efb8
yallu201 <dlruddlf07@gmail.com>
2021-10-01 23:29:19 +0900

    Updates

--- src/modules/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import block from "./block";
-import board from "./board";
-import game from "./game";
-
-const rootReducer = combineReducers({ block, board, game });
-
-export default rootReducer;
